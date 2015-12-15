8b3f5fb618b85ea5d8c40dc9e67057508b91a6cc
NGAEVA <ywt1230@163.com>
2015-12-14 20:15:49 -0800

    Make commit randomly

--- HelloWorld/Source.cpp
+++ HelloWorld/Source.cpp
@@ -1,10 +1,43 @@
 #include <iostream>
-
 void main()
 {
-	std::cout << "First commit\n";
-	system("pause");
+	int c;
+	c = rand() % 6+1;
+	//make commit randomly
+	while (1)
+	{
+		if (c == 1)
+		{
+			std::cout << "First commit\n";
+			system("pause");
+		}
 
-	std::cout << "Second commit\n";
-	system("pause");
+		else if (c == 2)
+		{
+			std::cout << "Second commit\n";
+			system("pause");
+		}
+		else if (c == 3)
+		{
+			std::cout << "Third commit\n";
+			system("pause");
+		}
+		else if (c == 4)
+		{
+			std::cout << "Fourth commit\n";
+			system("pause");
+		}
+
+		else if (c == 5)
+		{
+			std::cout << "Fifth commit\n";
+			system("pause");
+		}
+		else
+		{
+			std::cout << "Commit done\n";
+			system("pause");
+		}
+	}
+	
 }
